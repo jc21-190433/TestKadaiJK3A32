aac6da02c149a608cf246c45d1cee2c38274b91c
敬幸 平山 <190433@jc-21.jp>
2022-02-15 14:13:05 +0900

    プロジェクト ファイルを追加します。

---
+++ JetCoaster.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+
+  
+        public class JetCoaster
+        {
+            public static int ride
+                (int k, int[] g, int r)
+            {
+                int[] cap = g;
+                int[] newcap = new int[g.Length];
+                int boa = 0;
+                int ret = 0;
+
+                for (int a = 0; a < r; a++)
+                {
+                    int count = 0;
+                    for (int b = 0; b < cap.Length; b++)
+                    {
+                        int boac = boa;
+                        boa += cap[b];
+                        count++;
+                        if (boa > k)
+                        {
+                            boa = boac;
+                            count--;
+                            break;
+                        }
+                    }
+                    for (int c = 0; c < count; c++)
+                    {
+                        int bff = cap[0];
+                        Array.Copy(cap, 1, newcap, 0, cap.Length - 1);
+                        newcap[newcap.Length - 1] = bff;
+
+                        cap = newcap;
+                    }
+                    ret += boa;
+                    boa = 0;
+                }
+
+                return ret;
+            }
+        }
+    }
---
+++ kuriage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+   
+        public class kuriage
+        {
+            public static int kuriage10(int i)
+            {
+                int j;
+                int k;
+                j = i;
+
+                if (i < 0)
+                {
+                    j = i * -1;
+                }
+
+                if (j % 10 != 0)
+                {
+                    k = j % 10;
+                    j = j + 10 - k;
+                    if (i < 0)
+                    {
+                        j = j * -1;
+                    }
+                    i = j;
+
+                }
+                return i;
+            }
+        }
+    }
